7b654b67bb4eab46fda5a0b008438418a73509e0
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-06 00:31:00 -0300

    Se creó una clase alumno con algunos métodos que pide la prueba.

---
+++ alumno.rb
@@ -0,0 +1,38 @@
+# La clase alumno que contiene el nombre y sus notas.
+class Alumno
+  def initialize(nombre, notas)
+    @nombre_alumno = nombre
+    @notas_alumno = notas
+  end
+
+  def nombre=(nombre)
+    @nombre_alumno = nombre
+  end
+
+  def nombre
+    @nombre_alumno
+  end
+
+  def notas=(notas)
+    @notas_alumno = notas
+  end
+
+  def notas
+    @notas_alumno
+  end
+
+  def promedio_notas
+    total = @notas_alumno.inject do |sum, n|
+      if n == 'A'
+        sum + 1
+      else
+        sum + n
+      end
+    end
+    total / @notas_alumno.length
+  end
+
+  def alumno_aprobado
+    promedio_notas >= 5
+  end
+end

f7ba907d3752add69ae42ed34f0dc35841d781a3
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-07 18:13:00 -0300

    Se dejo '' en vez de "".

--- main.rb
+++ main.rb
@@ -8,7 +8,8 @@ require 'csv'
 #   file.push(row.to_s)
 # end
 # print file
-file = File.open("./notas_alumnos.csv", "r")
+file = File.open('./notas_alumnos.csv', 'r')
 contents = file.readlines
 file.close
 print contents
+puts ''

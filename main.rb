28310433033cd7a77b6315a93b1f133cfb7c06d6
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-07 10:32:17 -0300

    Se agrega archivo.

--- main.rb
+++ main.rb
@@ -1,3 +1,9 @@
 require_relative 'files.rb'
 require_relative 'methods.rb'
 require_relative 'program.rb'
+
+file = File.read('notas_alumnos.csv', 'r')
+contents = file
+print contents
+puts ''
+file.close

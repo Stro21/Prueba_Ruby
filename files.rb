43bc15925766610d8bf6e95445528aab098df560
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-07 19:01:01 -0300

    Lee utilizando una función.

--- files.rb
+++ files.rb
@@ -1,2 +1,9 @@
 require_relative 'methods.rb'
 require_relative 'program.rb'
+
+def read_file
+  file = File.open('./notas_alumnos.csv', 'r')
+  contents = file.readlines
+  file.close
+  contents
+end
--- main.rb
+++ main.rb
@@ -3,13 +3,7 @@ require_relative 'methods.rb'
 require_relative 'program.rb'
 require 'csv'
 
-# file = []
-# CVS.foreach('notas_alumnos.csv') do |row|
-#   file.push(row.to_s)
-# end
-# print file
-file = File.open('./notas_alumnos.csv', 'r')
-contents = file.readlines
-file.close
-print contents
+file = read_file
+print file
 puts ''
+puts file[0]

1b4ce143aab8ab61251cdaeffa5275376bcfc192
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-08 10:41:42 -0300

    Funciona promedios y inasistencias.

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require_relative 'files.rb'
 require_relative 'methods.rb'
 require_relative 'program.rb'
 
+# rubocop:disable LineLength
+
 file = read_file
 alumnos = create_array_hash(file)
 print alumnos
@@ -10,3 +12,6 @@ llaves = alumnos.keys
 notas = alumnos.values
 puts 'Promedio de ' + llaves[0] + ' es ' + promedio(notas[0]).to_s
 puts 'Promedio de ' + llaves[1] + ' es ' + promedio(notas[1]).to_s
+puts 'Cantidad de inasistencias de ' + llaves[2] + ' es ' + inasistencia(notas[2]).to_s
+
+# rubocop:enable LineLength

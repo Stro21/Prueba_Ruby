03669e69853015fbd5ff29b3ce0b77553dcabab4
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-07 23:39:46 -0300

    Se esta probando.

--- main.rb
+++ main.rb
@@ -5,6 +5,6 @@ require_relative 'program.rb'
 file = read_file
 print file
 puts ''
-alumnos = create_array_hash(file)
-print alumnos
-puts ''
+# alumnos = create_array_hash(file)
+# print alumnos
+# puts ''

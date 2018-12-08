88c0314f19a7b704813e52d7b7fef3b919e0a51b
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-08 12:44:01 -0300

    Las opciones 2 y 3 funcionan.

--- main.rb
+++ main.rb
@@ -6,9 +6,4 @@ file = read_file
 alumnos = create_array_hash(file)
 print alumnos
 puts ''
-llaves = alumnos.keys
-notas = alumnos.values
-puts 'Promedio de ' + llaves[0] + ' es ' + promedio(notas[0]).to_s
-puts 'Promedio de ' + llaves[1] + ' es ' + promedio(notas[1]).to_s
-inasistencias_totales(llaves[2], notas[2])
-alumnos_aprobados(alumnos)
+inicio_programa(alumnos)
--- program.rb
+++ program.rb
@@ -3,39 +3,39 @@
 require_relative 'methods.rb'
 require_relative 'files.rb'
 
-def prorama(opt, hash)
+def programa(opt, arr_hash)
   if opt == 1
-    option1(hash)
+    option1(arr_hash)
   elsif opt == 2
-    option2(hash)
+    option2(arr_hash)
   elsif opt == 3
-    option3(hash)
+    option3(arr_hash)
   elsif opt == 4
     puts 'Se salio del programa.'
     exit(0)
   else
     puts 'Opción incorrecta'
-    inicio_programa(hash)
+    inicio_programa(arr_hash)
   end
 end
 
-def inicio_programa(hash)
-  puts 'Ingrese una opción del 1 al 4 (la opción 4 es salir del programa'
+def inicio_programa(arr_hash)
+  puts 'Ingrese una opción del 1 al 4 (la opción 4 es salir del programa).'
   opt = gets.chomp.to_i
-  programa(opt, hash)
+  programa(opt, arr_hash)
 end
 
 # Cuenta las inasistencias totales
-def option2(hash)
+def option2(arr_hash)
   puts 'Las inasistencias totales de los alumnos son: '
-  hash.each { |nombre, notas| inasistencias_totales(nombre, notas) }
-  inicio_programa(hash)
+  arr_hash.each { |nombre, notas| inasistencias_totales(nombre, notas) }
+  inicio_programa(arr_hash)
 end
 
 # Muestra los alumnos aprobados
-def option3(hash)
-  alumnos_aprobados(hash)
-  inicio_programa(hash)
+def option3(arr_hash)
+  alumnos_aprobados(arr_hash)
+  inicio_programa(arr_hash)
 end
 
 # rubocop:enable MethodLength
